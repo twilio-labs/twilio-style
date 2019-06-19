0c958f7f189b6325dea66f2a785ff41dcd9b2471
Alice Cai <acai@twilio.com>
2019-06-18 17:01:12 -0700

    add possible errors, best practices, variables, and node rules

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,18 @@
-const style = require('./rules/style');
+const possibleErrors = require('./rules/possible-errors');
+const bestPractices = require('./rules/best-practices');
+const variables = require('./rules/variables');
+const node = require('./rules/node');
+const stylisticIssues = require('./rules/stylistic-issues');
 
 module.exports = {
-	extends: ['eslint:recommended', style],
+	extends: [
+		'eslint:recommended',
+		possibleErrors,
+		bestPractices,
+		variables,
+		node,
+		stylisticIssues,
+	],
 	rules: {
 		strict: ['error', 'never'],
 	},
---
+++ src/js/rules/possible-errors.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules: {
+        'no-await-in-loop': 'off',
+        'no-console': 'error',
+        'no-extra-parens': ['error', 'all', { ignoreJSX: 'all' }],
+        'no-template-curly-in-string': 'error',
+    },
+};
