9eca1e62b150bb5f817fc1d9e1ffb23d4174f5fd
Alice Cai <ahcai@berkeley.edu>
2019-10-22 10:59:59 -0700

    update object-curly-newline minProperties to 3

--- packages/eslint-config-twilio/rules/stylistic-issues.js
+++ packages/eslint-config-twilio/rules/stylistic-issues.js
@@ -155,19 +155,19 @@ module.exports = {
       {
         ObjectExpression: {
           multiline: true,
-          minProperties: 2,
+          minProperties: 3,
         },
         ObjectPattern: {
           multiline: true,
-          minProperties: 2,
+          minProperties: 3,
         },
         ImportDeclaration: {
           multiline: true,
-          minProperties: 2,
+          minProperties: 3,
         },
         ExportDeclaration: {
           multiline: true,
-          minProperties: 2,
+          minProperties: 3,
         },
       },
     ],
