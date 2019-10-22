e3a138215c0d6d67eb0d6eab6e84dbc0a6b036f6
Alice Cai <ahcai@berkeley.edu>
2019-10-22 16:13:13 -0700

    move react settings out of the rules file

--- packages/eslint-config-twilio-react/index.js
+++ packages/eslint-config-twilio-react/index.js
@@ -3,5 +3,18 @@ module.exports = {
     'eslint-config-twilio',
     './rules/react',
   ].map(require.resolve),
+  plugins: [
+    'react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {},
 };
