f2afa9a942220200ac62b5ded4ed97d8c147e7b9
Kousha Talebian <kousha.talebian@gmail.com>
2019-10-28 10:39:24 -0700

    enable the ts rules

--- packages/eslint-config-twilio-ts/index.js
+++ packages/eslint-config-twilio-ts/index.js
@@ -2,11 +2,15 @@ module.exports = {
   extends: [
     'eslint-config-twilio',
     'plugin:@typescript-eslint/recommended',
+    './rules/typescript'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
       ecmaVersion: 2018,
       sourceType: 'module',
+      project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: [
+    '@typescript-eslint',
+  ],
 };
