127985040ace9d679e23fe1d626a6bc56e57ccd7
Alice Cai <ahcai@berkeley.edu>
2019-11-13 16:52:19 -0800

    update typescript settings to work with eslint-plugin-import

--- packages/eslint-config-twilio-ts/index.js
+++ packages/eslint-config-twilio-ts/index.js
@@ -1,5 +1,7 @@
 const rules = require('./rules');
 
+const allExtensions = ['.ts', '.tsx', '.d.ts', '.js', '.jsx'];
+
 module.exports = {
   extends: ['twilio', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
@@ -9,5 +11,16 @@ module.exports = {
     project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint'],
+  settings: {
+    'import/extensions': allExtensions,
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/resolver': {
+      'node': {
+        'extensions': allExtensions,
+      },
+    },
+  },
   rules,
 };
