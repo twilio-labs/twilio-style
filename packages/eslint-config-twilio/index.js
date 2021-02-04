43f3b39dea47acd1a9b24f5b80371730ab2a51f0
Kousha Talebian <kousha.talebian@gmail.com>
2021-02-04 09:58:43 -0800

    add babel-eslint and parser option to js (#103)

--- packages/eslint-config-twilio/index.js
+++ packages/eslint-config-twilio/index.js
@@ -5,6 +5,10 @@ const allExtensions = ['.ts', '.tsx', '.d.ts', '.js', '.jsx'];
 module.exports = {
   extends: ['twilio-base', 'prettier', 'plugin:sonarjs/recommended'],
   plugins: ['import', 'prettier', 'no-only-tests'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2019,
+  },
   settings: {
     'import/extensions': allExtensions,
     'import/ignore': ['node_modules', '\\.(coffee|scss|css|less|hbs|svg|json)$'],
